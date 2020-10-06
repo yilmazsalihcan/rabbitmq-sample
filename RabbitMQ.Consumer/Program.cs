a335c915a1dce4fed561a3b3f7969515f6cf971e
Salih Yılmaz <salihyilmaz@kod.com.tr>
2020-10-06 22:18:40 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnanjfbl:FjuLTzMb--fve1OuKcp3Ar02gdt-JGMw@wasp.rmq.cloudamqp.com/wnanjfbl");
+            //factory.HostName = "localhost";
+          
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume("hello", true, consumer);
+
+                    consumer.Received += (model, ev) =>
+                    {                        
+                        var message = Encoding.UTF8.GetString(ev.Body.ToArray());
+                        Console.WriteLine("Mesaj alındı" + message);
+                    };
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız...");
+                Console.ReadLine();
+            }
+        }
+
+       
+    }
+}
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnanjfbl:FjuLTzMb--fve1OuKcp3Ar02gdt-JGMw@wasp.rmq.cloudamqp.com/wnanjfbl");
+            //factory.HostName = "localhost";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello World";
+                    var bodyByte = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", routingKey: "hello", null, bodyByte);
+                    Console.WriteLine("Mesajınız gönderilmiştir.");
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız...");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
